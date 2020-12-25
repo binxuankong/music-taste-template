f5af89ea927c5c36644bb9f605ebc592569b9d30
Bin Xuan Kong <binxuankong@gmail.com>
2020-12-25 16:35:56 +0800

    v1.0

--- app/main.py
+++ app/main.py
@@ -10,26 +10,25 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'CALIWASAMISSIONBUTNOWAGLEAVING'
 
 API_BASE = "https://accounts.spotify.com"
-# REDIRECT_URI = "http://music-taste-d.herokuapp.com/callback"
-REDIRECT_URI = "http://127.0.0.1:5000/callback"
+REDIRECT_URI = "http://music-taste-d.herokuapp.com/callback"
+# REDIRECT_URI = "http://127.0.0.1:5000/callback"
 SCOPE = "user-read-recently-played user-top-read"
 CLIENT_ID = "e08da5aa4af243e2ac533ca96170a077"
 CLIENT_SECRET = "116b8f9c375d467c9a1722af618923eb"
 
 @app.route('/')
 def index():
-    con = get_db_connection()
-    users = con.execute('SELECT * FROM users').fetchall()
-    con.close()
-    return render_template('index.html', users=users)
+    if 'token' in session:
+        return render_template('index.html', session=session)
+    return render_template('index.html')
 
 @app.route('/<int:user_id>')
 def user(user_id):
     user = get_user(user_id)
     return render_template('user.html', user=user)
 
-@app.route('/verify')
-def verify():
+@app.route('/link')
+def link():
     auth_url = f"{API_BASE}/authorize?client_id={CLIENT_ID}&response_type=code&redirect_uri={REDIRECT_URI}&scope={SCOPE}"
     return redirect(auth_url)
 
@@ -56,28 +55,5 @@ def profile():
         user_profile = get_user_profile(sp)
         top_artists = get_top_artists(sp)
         top_tracks = get_top_tracks(sp)
-        return render_template('profile.html', user=user_profile, artists=top_artists, tracks=top_tracks)
-
-@app.route('/link', methods=('GET', 'POST'))
-def link():
-    if request.method == 'POST':
-        sp = get_spotify_connection()
-        user_info = sp.me()
-    
-        if not user_info:
-            flash("Unable to access to user!")
-        else:
-            dname = user_info['display_name']
-            spotify_url = user_info['external_urls']['spotify']
-            image_url = user_info['images']['url']
-            con = get_db_connection()
-            con.execute("""INSERT INTO users (dname, spotify_url, image_url) VALUES (?, ?, ?)
-                        SELECT *
-                        WHERE NOT EXISTS (SELECT 1 FROM users WHERE spotify_url = '{}')
-                        """.format(spotify_url),
-                        (dname, spotify_url, image_url))
-            con.commit()
-            con.close()
-            return redirect(url_for('index'))
-
-    return render_template('link.html')
+        return render_template('profile.html', user=user_profile, artists=top_artists, tracks=top_tracks, session=session)
+    return render_template('profile.html')
